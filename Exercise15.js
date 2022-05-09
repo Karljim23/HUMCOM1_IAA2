cab93e5931f06f7f45f15663ab3e04b3f723061b
Karl Jim Garing <97873009+Karljim23@users.noreply.github.com>
2022-05-09 20:10:42 +0800

    Add files via upload

---
+++ Exercise15.js
@@ -0,0 +1,11 @@
+function displayAssignedValues(){
+    var firstName = document.getElementById("firstName").value = "Karl Jim Garing";
+    var age = document.getElementById("Age").value = "21"
+    var phoneNumber = document.getElementById("PhoneNumber").value = "0966-527-7694";
+    var eMail = document.getElementById("eMail").value = "20223717@s.ubaguio.edu";
+    var mailAddress = document.getElementById("mailAddress").value = "6107 Acasia St. Kias, Baguio City";
+    var program = document.getElementById("program").value = "Information Technology";
+    var idNum = document.getElementById("idNum").value = "20223717";
+    var yearLevel = document.getElementById("yearLevel").value = "1st Year";
+    var section = document.getElementById("section").value = "IAA2"
+}
